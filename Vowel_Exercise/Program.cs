138a83d9614818ddc1286c9b6e4ceee0426605c4
EldritchShroom <patrickmckay98@icloud.com>
2024-10-10 09:50:33 -0500

    Created vowel project
    Created the "Is it a vowel" folder and added the code to it. Need to debug

---
+++ Vowel_Exercise/Program.cs
@@ -0,0 +1,26 @@
+﻿
+using System.Runtime.CompilerServices;
+
+string input;
+
+Console.Write("Please enter a letter: ");
+input = Console.ReadLine();
+
+string letter = String.ToUpper(input);
+
+switch (letter)
+{
+    case A:
+    case E: 
+    case I:
+    case O:
+    case U: 
+        Console.WriteLine("Vowel");
+        break;
+    case Y:
+        Console.WriteLine("Maybe");
+        break;
+    default:
+        Console.WriteLine("Not a vowel");
+        break;
+}
