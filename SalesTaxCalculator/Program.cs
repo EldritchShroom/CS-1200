3ed3a10b85cb75a39c9a7f3084394fc6f195b4f6
EldritchShroom <patrickmckay98@icloud.com>
2024-10-11 12:14:03 -0500

    Add program outline
    Added a program outline and minor code.

--- SalesTaxCalculator/Program.cs
+++ SalesTaxCalculator/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+    Prompt the user for total purchase price
+    Prompt user for tax rate
+
+    Use proper format codes and string interpolation for currenct and other symbols
+
+    Result should be outputted with the purchase price, tax rate %, amount of tax, and the total owed.
+*/
+
+// Ask user for the sub_total amount before taxes
+Console.Write("Enter the purchase price: ");
+int sub_total = int.Parse(Console.ReadLine()):
+
+// Ask the user for the tax rate
+Console.Write("Enter the tax rate: "); 
+decimal tax_rate = decimal.Parse(Console.ReadLine()); // tax_rate needs to be set to a decimal and not an int
+
+// Calculate the of tax that needs to be paid, use the tax_rate and sub_total
+
+
+// Add the tax_amount to the sub_total amount for the final_total 
