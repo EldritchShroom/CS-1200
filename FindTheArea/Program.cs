b03df04011d4fe7143295c0743437ab3532ee979
EldritchShroom <patrickmckay98@icloud.com>
2024-10-13 09:31:01 -0500

    Update program file
    Add rough outline for the program, added first prompt for user input and switch statement.

--- FindTheArea/Program.cs
+++ FindTheArea/Program.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+ Ask user for shape they want area for.
+
+If user chose Rectangle, ask for width and length.
+
+If user chose Triangle, ask for base and height.
+
+if user chose Cirlce, ask for radius. 
+*/
+
+
+Console.Write("What type of shape (R)ectangle, (T)riangle, or (C)ircle? ");
+
+string shape = Console.ReadLine();
+
+switch(shape)
+{
+    case "R":
+    case "r":
+        // Ask user for width and length
+    
+    case "T":
+    case "t":
+        // Ask user for base and height
+    
+    case "C":
+    case "c":
+        // Ask user for radius
+    
+    default:
+        // Invalid input message or something, idk i just work here
+}
