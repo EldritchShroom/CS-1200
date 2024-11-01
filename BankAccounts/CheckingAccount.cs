fa78deb745ee555ac03a4b7d9f7baf6c2f09949e
EldritchShroom <patrickmckay98@icloud.com>
2024-11-01 14:11:44 -0500

    Add whiteboard notes to program

--- BankAccounts/CheckingAccount.cs
+++ BankAccounts/CheckingAccount.cs
@@ -5,5 +5,12 @@ namespace BankAccounts;
 
 public class CheckingAccount : Account
 {
-    
+    public override void Withdraw(decimal amount)
+    {
+        // _balance -= amount; this is what is in the normal code
+
+        // I could deduct the amount from the balance then check that it is still less than -100
+        //I could check if the smount exceeds the balance and then if it does how much does it exceed.
+        if 
+    }
 }
