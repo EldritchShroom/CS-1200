d9ab4c56bd7ad42910faebce1c1f4c8359802443
EldritchShroom <patrickmckay98@icloud.com>
2024-11-08 14:24:42 -0600

    Update HandleInput.cs
    Added code to the class to present the user with the welcome message and give them options.

--- HangMan/HangMan.UI/HandleInput.cs
+++ HangMan/HangMan.UI/HandleInput.cs
@@ -10,7 +10,19 @@ public class HangMan
 
         while (isTrue)
         {
-            Console.WriteLine("Welcome to Hangman")
+            Console.WriteLine("Welcome to Hangman!");
+            Console.WriteLine();
+
+            Console.Write("Enter your name: ");
+            Console.WriteLine();
+
+            Console.WriteLine("How would you like to choose your words?");
+
+            Console.Write("1. Enter the word in the console window.");
+            Console.Write("2. Pick a random word from the dictionary for me.");
+
+            int input = int.Parse(Console.ReadLine());
+
         }
     }
 }
