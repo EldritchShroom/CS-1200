67e3de63c40e771c87e765eae39b52c5fa58e3a9
EldritchShroom <patrickmckay98@icloud.com>
2024-11-09 17:10:03 -0600

    Whtieboard the Random word implementation
    And began by creating the dictionary.txt file.

--- HangMan/obj/Debug/net8.0/HangMan.AssemblyInfo.cs
+++ HangMan/obj/Debug/net8.0/HangMan.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HangMan")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5bec0c6b46f5d19dd3eff1010040ce952069d3e3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2e8bf06b84f77eb448f56f3fc696d8123bb5ebb5")]
 [assembly: System.Reflection.AssemblyProductAttribute("HangMan")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HangMan")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
