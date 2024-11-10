84623b151536e61234a01b3f633cecb6026f0bf9
EldritchShroom <patrickmckay98@icloud.com>
2024-11-10 14:51:34 -0600

    Add program to GuessHandling
    Added a class that asks user to choose how they want to get a word and returns their choice.

--- HangMan/HangMan.UI/GuessHandling.cs
+++ HangMan/HangMan.UI/GuessHandling.cs
@@ -4,7 +4,16 @@ public class HandleGuess
 {
     public int GuessChoice()
     {
-        Console.WriteLine("How would you like to choose your words?");
         
+        Console.WriteLine("How would you like to choose your words?");
+        Console.WriteLine();
+        Console.WriteLine("1. Enter the word in the console window.");
+        Console.WriteLine("2. Pick a random word from the dictionary for me.");
+
+        Console.Write("Enter choice: ");
+
+        int input = int.Parse(Console.ReadLine());
+
+        return input;
     }
 }
--- HangMan/HangMan.UI/HandleInput.cs
+++ HangMan/HangMan.UI/HandleInput.cs
@@ -13,16 +13,21 @@ public class HangMan
             Console.WriteLine("Welcome to Hangman!");
             Console.WriteLine();
 
+            /*
+            This part was put into "AskForName.cs"
             Console.Write("Enter your name: ");
             Console.WriteLine();
+            */
 
+            /* 
+            This part was put into "GuessHandling.cs"
             Console.WriteLine("How would you like to choose your words?");
 
             Console.Write("1. Enter the word in the console window.");
             Console.Write("2. Pick a random word from the dictionary for me.");
 
             int input = int.Parse(Console.ReadLine());
-
+            */
         }
     }
 }
